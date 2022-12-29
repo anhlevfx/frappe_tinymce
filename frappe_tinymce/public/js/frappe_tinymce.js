23547fd737c8ffa3b4f899c10ece4daf8fac7a1e
Anh Le <anhle>
2022-12-29 17:02:05 +0700

    force not override absolutely domain

--- frappe_tinymce/public/js/frappe_tinymce.js
+++ frappe_tinymce/public/js/frappe_tinymce.js
@@ -20,9 +20,9 @@ frappe.ui.form.ControlTextEditor = class ControlTextEditor extends frappe.ui.for
             toolbar: 'undo redo | bold italic underline strikethrough | fontfamily fontsize blocks | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist checklist | forecolor backcolor casechange permanentpen formatpainter removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media pageembed template link anchor codesample | a11ycheck ltr rtl | showcomments addcomment | footnotes | mergetags',
             font_size_formats: '10px 11px 12px 14px 15px 16px 18px 24px 36px',
             plugins: [
-              'autoresize', 'autolink', 'charmap', 'emoticons', 'fullscreen', 'help',
-              'image', 'link', 'lists', 'searchreplace',
-              'table', 'visualblocks', 'visualchars', 'wordcount',
+                'autoresize', 'autolink', 'charmap', 'emoticons', 'fullscreen', 'help',
+                'image', 'link', 'lists', 'searchreplace',
+                'table', 'visualblocks', 'visualchars', 'wordcount',
             ],
             powerpaste_googledocs_import: "prompt",
             entity_encoding: 'raw',
@@ -30,10 +30,12 @@ frappe.ui.form.ControlTextEditor = class ControlTextEditor extends frappe.ui.for
             content_css: false,
             toolbar_sticky: true,
             promotion: false,
+            relative_urls: false,
+            remove_script_host: false,
 
-            setup: function(editor) {
+            setup: function (editor) {
                 that.editor_id = editor.id
-                editor.on('Change', function(e) {
+                editor.on('Change', function (e) {
                     that.parse_validate_and_set_in_model(e.level.content);
                 });
                 editor.on('init', function (e) {
@@ -44,11 +46,11 @@ frappe.ui.form.ControlTextEditor = class ControlTextEditor extends frappe.ui.for
         this.activeEditor = tinymce.activeEditor
     }
 
-    set_formatted_input(value){
-        if (this.frm && !this.frm.doc.__setContent){
-            if(value){
+    set_formatted_input(value) {
+        if (this.frm && !this.frm.doc.__setContent) {
+            if (value) {
                 this.activeEditor.setContent(value)
-            }else{
+            } else {
                 this.activeEditor.setContent("")
             }
         }
